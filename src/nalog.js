d8c72f86a70d6a27dc722ad53270e36c013096d7
kicumkicum <kicumkicum@gmail.com>
2017-06-26 23:04:53 +0700

    Fix codestyle

--- src/nalog.js
+++ src/nalog.js
@@ -45,79 +45,79 @@ const SERVICE_BASE_URL = 'https://service.nalog.ru/inn.do';
  * @this {Nalog}
  */
 var Nalog = function(deps) {
-  this._request = deps.request;
-
-  /**
-   * @param {Nalog.InputData} data
-   * @param {{
-   *   token: string,
-   *   value: string
-   * }} captcha
-   * @this {Nalog}
-   */
-  this.getINN = function(data, captcha) {
-	const url = 'https://service.nalog.ru/inn-proc.do';
-	const newData = this._createOutData(data, captcha);
-	const responce = this._request.fetch('post', url, newData);
-  };
-
-  this.getCaptchaImageSrc = function() {
-	const captchaToken = this._loadCaptchaToken();
-	return this._loadCaptchaImageSrc(captchaToken.loadTime, captchaToken.token);
-  };
+	this._request = deps.request;
 
 	/**
-	 *  @param {Nalog.InputData} data
+	 * @param {Nalog.InputData} data
 	 * @param {{
-   *   token: string,
-   *   value: string
-   * }} captcha
+	 *	 token: string,
+	 *	 value: string
+	 * }} captcha
+	 * @this {Nalog}
+	 */
+	this.getINN = function(data, captcha) {
+		const url = 'https://service.nalog.ru/inn-proc.do';
+		const newData = this._createOutData(data, captcha);
+		const responce = this._request.fetch('post', url, newData);
+	};
+
+	this.getCaptchaImageSrc = function() {
+		const captchaToken = this._loadCaptchaToken();
+		return this._loadCaptchaImageSrc(captchaToken.loadTime, captchaToken.token);
+	};
+
+	/**
+	 * @param {Nalog.InputData} data
+	 * @param {{
+	 *	 token: string,
+	 *	 value: string
+	 * }} captcha
 	 * @return {Nalog.OutputtData}
 	 * @private
 	 */
-  this._createOutData = function(data, captcha) {
-	  return {
-		  'fam': data.fam,
-		  'nam': data.nam,
-		  'otch': data.otch,
-		  'bdate': data.bdate,
-		  'docno': data.docno,
-		  'docdt': data.docdt,
-		  'captcha': captcha.value,
-		  'captchaToken': captcha.token,
-		  'c': 'innMy',
-		  'bplace': '',
-		  'doctype': '21'
-	  };
-  };
+	this._createOutData = function(data, captcha) {
+		return {
+			'fam': data.fam,
+			'nam': data.nam,
+			'otch': data.otch,
+			'bdate': data.bdate,
+			'docno': data.docno,
+			'docdt': data.docdt,
+			'captcha': captcha.value,
+			'captchaToken': captcha.token,
+			'c': 'innMy',
+			'bplace': '',
+			'doctype': '21'
+		};
+	};
 
 
-  /**
-   * @param {number} loadTime
-   * @param {string} token
-   * @return {string}
-   * @private
-   * @this {Nalog}
-   */
-  this._loadCaptchaImageSrc = function(loadTime, token) {
+	/**
+	 * @param {number} loadTime
+	 * @param {string} token
+	 * @return {string}
+	 * @private
+	 * @this {Nalog}
+	 */
+	this._loadCaptchaImageSrc = function(loadTime, token) {
 	return CAPTCHA_BASE_URL + '?r=' + loadTime + '&a=' + token;
-  };
+	};
 
-  /**
-   * @return {{loadTime: number, token: string}}
-   * @private
-   */
-  this._loadCaptchaToken = function() {
+	/**
+	 * @return {{loadTime: number, token: string}}
+	 * @private
+	 */
+	this._loadCaptchaToken = function() {
 	const now = Math.floor(Date.now() / 1000);
 	const response = UrlFetchApp.fetch(CAPTCHA_BASE_URL + '?' + now).getContentText();
 	return {loadTime: now, token: response};
-  };
+	};
 };
 
 
 /**
  * @typedef {{
- *   request: Request
+ *	 request: Request
  * }}
  */
 Nalog.Deps;
@@ -125,12 +125,12 @@ Nalog.Deps;
 
 /**
  * @typedef {{
- *   fam: string,
- *   nam: string,
- *   otch: string,
- *   bdate: string,
- *   docno: string,
- *   docdt: string
+ *	 fam: string,
+ *	 nam: string,
+ *	 otch: string,
+ *	 bdate: string,
+ *	 docno: string,
+ *	 docdt: string
  * }}
  */
 Nalog.InputData;
@@ -138,17 +138,17 @@ Nalog.InputData;
 
 /**
  * @typedef {{
- *   fam: string,
- *   nam: string,
- *   otch: string,
- *   bdate: string,
- *   docno: string,
- *   docdt: string,
- *   captcha: string,
- *   captchaToken: string,
- *   c: string,
- *   bplace: string,
- *   doctype: string
+ *	 fam: string,
+ *	 nam: string,
+ *	 otch: string,
+ *	 bdate: string,
+ *	 docno: string,
+ *	 docdt: string,
+ *	 captcha: string,
+ *	 captchaToken: string,
+ *	 c: string,
+ *	 bplace: string,
+ *	 doctype: string
  * }}
 */
 Nalog.OutputtData;
