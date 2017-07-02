0984cd6f96a7e10e88ee5f9f29feffd3648122fb
kicumkicum <kicumkicum@gmail.com>
2017-07-03 00:59:36 +0700

    Set inn to sheet

--- src/index.js
+++ src/index.js
@@ -9,8 +9,17 @@ function onOpen() {
 /**
  * @param {string} captcha
  */
-function setCaptcha(captcha) {
-	app.setCaptcha(captcha);
+function __setCaptcha(captcha) {
+	app.writeINN(captcha);
+}
+
+
+/**
+ * @param {...*} var_args
+ */
+function log(var_args) {
+	const args = [].join.call(arguments, ', ');
+	Logger.log(args);
 }
 
 
@@ -18,9 +27,12 @@ function setCaptcha(captcha) {
  * @private
  */
 function _getINN() {
-	app.getINN();
+	app.getCaptcha();
 }
 
+function testReadRow() {
+	app.writeINN();
+}
 
 var App = function() {
 	/**
@@ -28,6 +40,12 @@ var App = function() {
 	 * @private
 	 */
 	this._ui = createUI();
+
+	/**
+	 * @type {Sheet}
+	 * @private
+	 */
+	this._sheet = new Sheet();
 
 	/**
 	 * @type {Request}
@@ -44,14 +62,30 @@ var App = function() {
 	/**
 	 * @this {App}
 	 */
-	this.getINN = function() {
+	this.getCaptcha = function() {
 		const captchaSrc = this._nalog.getCaptchaImageSrc();
 		const currentApp = SpreadsheetApp.getActiveSpreadsheet();
 		this._ui.showCaptcha(captchaSrc, currentApp);
+
+		// Next call this.setCaptcha
 	};
 
-	this.setCaptcha = function(captcha) {
-		Logger.log(captcha);
+	this.writeINN = function(captcha) {
+		// Read fio from current Row
+		const value = this._sheet.readFromActiveRow();
+
+		// Send POST request to Nalog for getting INN
+		const inn = this._nalog.getINN({
+			fam: value[0],
+			nam: value[1],
+			otch: value[2],
+			bdate: value[3],
+			docno: value[5],
+			docdt: value[6]
+		}, captcha);
+
+		// Write INN to Cell
+		this._sheet.writeToCell(inn, 'H' + this._sheet.getCurrentRow());
 	};
 
 	this.createMenu = function() {
