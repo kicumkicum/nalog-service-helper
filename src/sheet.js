81d87d902ab6fcf59293e7e97f8d78d6abe481d5
kicumkicum <kicumkicum@gmail.com>
2017-07-03 23:54:52 +0700

    Add enum `Sheet.RowName`

--- src/sheet.js
+++ src/sheet.js
@@ -1,3 +1,7 @@
+/**
+ * @global
+ * @constructor
+ */
 function Sheet() {
 	/**
 	 * @type {GoogleAppsScript.Spreadsheet.SpreadsheetApp}
@@ -12,10 +16,23 @@ function Sheet() {
 		return a[0];
 	};
 
+	/**
+	 * @param {string|number} value
+	 * @param {{
+	 *      row: number,
+	 *      column: number
+	 * }} position
+	 * @this {Sheet}
+	 */
 	this.writeToCell = function(value, position) {
-		this._app.getActiveSheet().getRange(position).setValue(value);
+		log('write to cell', value, position.row, position.column);
+		this._app.getActiveSheet().getRange(position.row, position.column).setValue(value);
 	};
 
+	/**
+	 * @return {number}
+	 * @this {Sheet}
+	 */
 	this.getCurrentRow = function() {
 		return this._app.getActiveRange().getRow();
 	};
@@ -25,3 +42,24 @@ function Sheet() {
 function test() {
 	(new Sheet()).readFromActiveRow();
 }
+
+
+/**
+ * @enum {number}
+ */
+Sheet.RowName = {
+	NVL: 0,
+	SQUARE: 1,
+	CON_DESC: 2,
+	REG_NO: 3,
+	TYPE: 4,
+	FULL_NAME: 5,
+	FAMILY: 6,
+	NAME: 7,
+	SECOND_NAME: 8,
+	BIRTHDAY: 9,
+	INN: 10,
+	PASSPORT_NUMBER: 11,
+	PASSPORT_DATE: 12,
+	SNILS: 13
+};
