6a80be63c0009d76a27a7cd0a84589cd7600ccb0
kicumkicum <kicumkicum@gmail.com>
2017-07-08 06:33:36 +0700

    Move src/bidge.js to index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = {};
+function onOpen() {
+	global.onOpen();
+}
+
+function setCaptcha(captcha) {
+	global.setCaptcha(captcha);
+}
+
+function log(var_args) {
+	const args = [].join.call(arguments, ', ');
+	Logger.log(args);
+}
+
+function getINN() {
+	global.getINN();
+}
+
+function splitNames() {
+	global.splitNames();
+}
